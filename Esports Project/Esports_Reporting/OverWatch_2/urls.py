de625635c7b51a00f0ecab3cbbe2353279ef4be8
johnjackhohl <66882162+johnjackhohl@users.noreply.github.com>
2023-09-01 12:13:45 -0400

    started three more map types

--- Esports Project/Esports_Reporting/OverWatch_2/urls.py
+++ Esports Project/Esports_Reporting/OverWatch_2/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Create_OW_Team, OW_Roster, OW_Roster_Players, Add_Player_to_Roster, Add_Match, Add_Game, Add_Control
+from .views import Create_OW_Team, OW_Roster, OW_Roster_Players, Add_Player_to_Roster, Add_Match, Add_Game, Add_Control, Add_Escort_Hybrid, Add_Flashpoint, Add_Push
 
 urlpatterns = [
     path('createTeam/', Create_OW_Team, name='create-team'),
@@ -8,5 +8,8 @@ urlpatterns = [
     path('team/<int:pk>/addPlayer/', Add_Player_to_Roster, name='add-player'),
     path('team/<int:pk>/addMatch/', Add_Match, name='add-match'),
     path('match/<int:pk>/addGame/', Add_Game, name='add-game'),
-    path('game/<int:pk>/addControl/', Add_Control, name='add-control')
+    path('game/<int:pk>/addControl/', Add_Control, name='add-control'),
+    path('game/<int:pk>/addEscortHybrid/', Add_Escort_Hybrid, name='add-escort-hybrid'),
+    path('game/<int:pk>/addFlashpoint/', Add_Flashpoint, name='add-flashpoint'),
+    path('game/<int:pk>/addPush/', Add_Push, name='add-push')
 ]
